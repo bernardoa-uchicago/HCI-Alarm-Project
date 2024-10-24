4568643103f3204a40202de6d549cba01debefef
Bernardo Albuquerque <bernardoa@uchicago.edu>
2024-10-23 22:10:23 -0500

    trial 2 done without csv

--- trialfinal.py
+++ trialfinal.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import font
 from datetime import datetime
+import pandas as pd
 import random
 from time import sleep
 from pydub import AudioSegment
@@ -17,9 +18,14 @@ window = tk.Tk()
 window.title("Latency Test")
 window.minsize(750, 500)
 window.configure(background=background)
+global result1_string, result2_string
+result1_string = ""
+result2_string = ""
 
 
 def title():
+    for widget in window.winfo_children():
+        widget.destroy()
     title_label = tk.Label(window, text="Welcome to the Beep Beep Trials!")
     title_label.config(font=("Times", 35, "bold"), background='black', fg='#69a8c9')
 
@@ -55,7 +61,7 @@ def title():
     txt.config(font=("Times", 20), background='black', fg='white', pady=20)
 
 
-    b1 = tk.Button(window, text="CONTINUE", font=('Times', 20, 'bold'), fg='blue', command=lambda: instructions(name_entry.get(), age_entry.get(), condition_entry.get(), neurodivergent_entry.get(), musician_entry.get))
+    b1 = tk.Button(window, text="CONTINUE", font=('Times', 20, 'bold'), fg='blue', command=lambda: instructions2(name_entry.get(), age_entry.get(), condition_entry.get(), neurodivergent_entry.get(), musician_entry.get))
     b3 = tk.Button(window, text='EXIT', font=('Times', 20, 'bold'), fg='red', command=window.destroy)
 
     title_label.pack()
@@ -75,8 +81,6 @@ def title():
     box5_label.place(x=300, y=300)
     musician_entry.place(x=300, y=350)
    
-
-
     txt.place(x=170, y=400)
 
     b1.place(x=200, y=450)
@@ -102,7 +106,6 @@ def play_alarm(alarm_name):
         alarm = AudioSegment.from_wav("alarm.wav")
         play(alarm)
         sleep(5)
-        print("old alarm done!")
     elif alarm_name == 'new':
         rising_thread = threading.Thread(target=generate_sinewave, args=('rise', ))
         falling_thread = threading.Thread(target=generate_sinewave, args=('fall', ))
@@ -112,7 +115,6 @@ def play_alarm(alarm_name):
 
         rising_thread.join()
         falling_thread.join()
-        print("new alarm done!")
         sleep(5)
 
 def alarm_randomizer():
@@ -121,16 +123,13 @@ def alarm_randomizer():
         number = random.randint(1, 2)  
         if number not in alarm_order:  
             alarm_order.append(number) 
-    print("DEBUG ORDER OF TESTS:")
-    print(alarm_order)
-    for pos in alarm_order:
-        if pos == 1:
-            play_alarm("provided")
-        elif pos == 2:
-            play_alarm("new")
+    return alarm_order
     
 
-def instructions(name_text, age_text, condition_text, neurodivergent_text, musician_text):
+def instructions2(name_text, age_text, condition_text, neurodivergent_text, musician_text):
+    global result1_string, result2_string
+    result1_string = result1_string + str(name_text) + ',' + str(age_text) + ',' + str(condition_text) + ',' + str(neurodivergent_text) + ',' + str(musician_text) + ','
+    result2_string = result2_string + str(name_text) + ',' + str(age_text) + ',' + str(condition_text) + ',' + str(neurodivergent_text) + ',' + str(musician_text) + ','
     for widget in window.winfo_children():
         widget.destroy()
 
@@ -140,7 +139,7 @@ def instructions(name_text, age_text, condition_text, neurodivergent_text, music
     Dscrp = tk.Label(window, text='You will hear two alarm sounds in order.\n\nEach sound will be played at a random time between 1 to 2 minutes \nfrom the moment you press [READY] or from the end of the first sound\n\nYou will then vote which sound startled you the most\n\n\nPress [READY] to begin')
     Dscrp.config(font=("Times", 20), background='black', fg='white', pady=10)
 
-    b1 = tk.Button(window, text="READY", font=('Times', 40, 'bold'), pady=30, padx=40, fg='green', command=lambda: alarm_randomizer())
+    b1 = tk.Button(window, text="READY", font=('Times', 40, 'bold'), pady=30, padx=40, fg='green', command=lambda: trial())
 
     alert.pack()
     Dscrp.pack()
@@ -149,5 +148,45 @@ def instructions(name_text, age_text, condition_text, neurodivergent_text, music
 def trial():
     for widget in window.winfo_children():
         widget.destroy()
+    window.update()
+    order = alarm_randomizer()
+    order_copy = order.copy()
+    while len(order):
+        print("sleeping")
+        sleep(random.randint(5,10))
+        if order.pop() == 1:
+            print("old")
+            play_alarm("provided")
+        else:
+            print("new")
+            play_alarm('new')
+
+    print("here")
+    rating1_label = tk.Label(window, text="Rate the first sound from 1 to 7 on how startling it was.")
+    rating1_label.config(font=("Times", 20), background=background, fg='#D3D3D3', pady=5)
+    rating1_entry = tk.Entry(window, font=("Arial", 14))
+
+    rating2_label = tk.Label(window, text="Rate the second sound from 1 to 7 on how startling it was.")
+    rating2_label.config(font=("Times", 20), background=background, fg='#D3D3D3', pady=5)
+    rating2_entry = tk.Entry(window, font=("Arial", 14))
+
+    replay = tk.Button(window, text="RETRY", font=('Times', 20, 'bold'), fg='red', command=trial)
+    ret = tk.Button(window, text="TITLE", font=('Times', 20, 'bold'), fg='black', command=lambda: save(order_copy, rating1_entry, rating2_entry))
+
+    rating1_label.pack()
+    rating1_entry.pack()
+    rating2_label.pack()
+    rating2_entry.pack()
+    replay.pack()
+    ret.pack()
+    window.update()
+
+def save(order_copy, rating1_entry, rating2_entry):
+    if order_copy.pop() == 1:
+        t2_res = str(rating1_entry) + ',' + str(rating2_entry)
+    else:
+        t2_res = str(rating2_entry) + ',' + str(rating1_entry)
+    results = pd.DataFrame(columns=["Name","Age","Auditory Condition","Neurodiv.","Musician","Old Rating","New Rating"])
+    print(results)
 
 title()
